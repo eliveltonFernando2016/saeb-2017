71403b9e99d7f1d6de0a7f24a09b002c58921c9e
Lucas Guedes Barboza <lucasguedesbarboza@gmail.com>
2019-06-12 16:32:11 -0300

    Add downloader module.
    On program execution, the downloader checks if the directory saeb-2017 exists. If not, this module downloads de .zip file from the SAEB webserver and extracts it to the corresponding directory.

---
+++ downloader.py
@@ -0,0 +1,33 @@
+import zipfile
+import requests
+import sys
+import os
+
+if not os.path.isdir("saeb-2017"):
+
+    link = "https://www.sample-videos.com/zip/10mb.zip"
+    file_name = "saeb2017.zip"
+
+    with open(file_name, "wb") as f:
+            print("Downloading {}".format(file_name))
+            response = requests.get(link, stream=True)
+            total_length = response.headers.get('content-length')
+
+            if total_length is None: # no content length header
+                f.write(response.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for data in response.iter_content(chunk_size=4096):
+                    dl += len(data)
+                    f.write(data)
+                    done = int(50 * dl / total_length)
+                    sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
+                    sys.stdout.flush()
+    print("\n")
+
+    zip_ref = zipfile.ZipFile(file_name, 'r')
+    zip_ref.extractall("saeb-2017")
+    zip_ref.close()
+
+    os.remove("saeb2017.zip")
